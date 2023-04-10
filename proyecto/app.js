7ba652ec8edb2e51b00ffc12b9a7000ecfcec084
isayukelson <iyukelson@udesa.edu.ar>
2023-04-10 09:32:12 -0300

    cambios

--- proyecto/app.js
+++ proyecto/app.js
@@ -12,6 +12,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+/* en estas lineas se requiren los modulos propios de rutas*/
+
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
@@ -21,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
