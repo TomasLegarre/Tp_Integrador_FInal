92b28ef0adb7c08c3b7f1ff7da588f6916096680
TomasLegarre <tlegarrematera@udesa.edu.ar>
2023-04-10 10:27:20 -0300

    hecho

--- proyecto/app.js
+++ proyecto/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+let productslRouter = require('./routes/product')
 
 var app = express();
 // nuevo cambio
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/product', productslRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
