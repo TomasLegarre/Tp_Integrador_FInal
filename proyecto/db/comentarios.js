29bffbc9c4e5b953e1efe2a7711aacf2d1186b5c
isayukelson <iyukelson@udesa.edu.ar>
2023-04-11 13:55:25 -0300

    comentarios de tel

--- proyecto/db/comentarios.js
+++ proyecto/db/comentarios.js
@@ -5,7 +5,95 @@ const lista_comentarios = [
         producto_id: 1,
         comentario: "Muy buen producto",
         fecha_carga : "DD-MM-YYYY"
-    }
+    },
+    {
+        id: 2,
+        usuario_id: 2,
+        producto_id: 1,
+        comentario: "Muy buen producto",
+        fecha_carga : "DD-MM-YYYY"
+    },
+    {
+        id: 3,
+        usuario_id: 3,
+        producto_id: 1,
+        comentario: "Me gusto",
+        fecha_carga : "DD-MM-YYYY"
+    },
+    {
+        id: 4,
+        usuario_id: 4,
+        producto_id: 1,
+        comentario: "Malisimo",
+        fecha_carga : "DD-MM-YYYY"
+    },
+    {
+        id: 4,
+        usuario_id: 4,
+        producto_id: 2,
+        comentario: "Malisimo",
+        fecha_carga : "DD-MM-YYYY"
+    },
+    {
+        id: 1,
+        usuario_id: 1,
+        producto_id: 3,
+        comentario: "Muy buen producto",
+        fecha_carga : "DD-MM-YYYY"
+    },
+    {
+        id: 2,
+        usuario_id: 2,
+        producto_id: 3,
+        comentario: "Muy buen producto",
+        fecha_carga : "DD-MM-YYYY"
+    },
+    {
+        id: 3,
+        usuario_id: 3,
+        producto_id: 3,
+        comentario: "Me gusto",
+        fecha_carga : "DD-MM-YYYY"
+    },
+    {
+        id: 4,
+        usuario_id: 4,
+        producto_id: 3,
+        comentario: "Me gusto",
+        fecha_carga : "DD-MM-YYYY"
+    },
+    {
+        id: 1,
+        usuario_id: 1,
+        producto_id: 4,
+        comentario: "Muy buen producto",
+        fecha_carga : "DD-MM-YYYY"
+    },
+    {
+        id: 2,
+        usuario_id: 2,
+        producto_id: 4,
+        comentario: "Muy buen producto",
+        fecha_carga : "DD-MM-YYYY"
+    },
+    {
+        id: 3,
+        usuario_id: 3,
+        producto_id: 4,
+        comentario: "Me gusto",
+        fecha_carga : "DD-MM-YYYY"
+    },
+    {
+        id: 4,
+        usuario_id: 4,
+        producto_id: 4,
+        comentario: "Me gusto",
+        fecha_carga : "DD-MM-YYYY"
+    },
+
+    
 ]
+
+
 
 module.exports = lista_comentarios
