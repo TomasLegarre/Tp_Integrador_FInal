0a1bfd2550ec11c763a60705bedfabd230647e99
victoriamoises03 <vmoises@udesa.edu.ar>
2023-06-14 15:55:16 -0300

    funciona add

--- proyecto/routes/product.js
+++ proyecto/routes/product.js
@@ -5,12 +5,12 @@ const controlador_product = require('../controllers/controlador_product');
 
 router.get('/add', controlador_product.product_add);
 router.get('/search-results', controlador_product.search_results);
-router.get('/edit', controlador_product.product_edit);
+router.get('/edit/:id', controlador_product.product_edit);
 
 router.post('/add', controlador_product.store);
 router.get('/detail/:id', controlador_product.product_detail);
 router.post('/delete/:id', controlador_product.product_delete);
-router.post('/edit', controlador_product.EditProduct);
+router.post('/edit/:id', controlador_product.EditProduct);
 
 module.exports = router;
 
